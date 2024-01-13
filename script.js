cf33787effb130b993e6b2f14e80e4c99f6515aa
Chick Chicky <91527355+ChickChicky@users.noreply.github.com>
2024-01-13 15:27:06 +0100

    Centered page
    * index.html is heavily broken

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
-const hm_button  = document.getElementById('hamburger-menu');
-const hm_close   = document.getElementById('close-hamburger-menu');
-const hm_bar     = document.getElementById('hamburger-menu-bar');
+const hm_button = document.getElementById('hamburger-menu');
+const hm_bar    = document.getElementById('hamburger-menu-bar');
 
 const get_projects = () => Array.from(document.getElementById('projects').children);
 
-hm_button.onclick = function() {
-    hm_bar.classList.remove('close-menu');
-    hm_bar.classList.add('open-menu');
-}
+let hm_open = false;
 
-hm_close.onclick = function() {
-    hm_bar.classList.remove('open-menu');
-    hm_bar.classList.add('close-menu');
+hm_button.onclick = function() {
+    if (hm_open) {
+        hm_bar.classList.add('close-menu');
+        hm_bar.classList.remove('open-menu');
+    } else {
+        hm_bar.classList.remove('close-menu');
+        hm_bar.classList.add('open-menu');
+    }
+    hm_open = !hm_open;
 }
